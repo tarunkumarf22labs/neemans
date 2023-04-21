a322d1c862d4c59305053cf5ed6ac4778c6008d2
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-04-21 18:03:08 +0530

    added f22 stories changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import  App  from './app'
 // import './index.css'
 
 define(
-	"f22-plugin",
+	"f22-stories",
 	(el) => <App dataURL={(el.getAttribute("dataURL") || "")}  
 	/>,
 	["dataURL" ],
 	() => console.log("F22 Plugin cleanup")
 );
-let el = document.createElement('f22-plugin')
-el.setAttribute('dataUrl', "https://f22videoplugin.s3.ap-northeast-1.amazonaws.com/exhibitionexperts/f22exhibition.json")
-document.body.append(el)
