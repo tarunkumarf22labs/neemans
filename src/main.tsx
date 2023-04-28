3930387d1e01a8da50752c46877f15fe3634c26c
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-04-28 16:15:52 +0530

    testing differnrtley

--- src/main.tsx
+++ src/main.tsx
@@ -11,19 +11,24 @@ define(
 
 
 
-async function loaderofdata() {
-  const data = await fetch("https://api.jsonbin.io/v3/b/644b7f5e9d312622a353b0e6");
-  const value = await data.json();
+// async function loaderofdata() {
+//   const data = await fetch("https://api.jsonbin.io/v3/b/644b7f5e9d312622a353b0e6");
+//   const value = await data.json();
   
-  value.record.forEach((element: { id: string; url: string }) => {
-    const mainelementsofstories = document.querySelector(`#${element.id}`);
-     if (mainelementsofstories) {
-      let el = document.createElement("f22-stories");
-      el.setAttribute("dataUrl", element.url);
-      mainelementsofstories?.appendChild(el);
-      document.body.append(mainelementsofstories!);
-     }
+//   value.record.forEach((element: { id: string; url: string }) => {
+//     const mainelementsofstories = document.querySelector(`#${element.id}`);
+//      if (mainelementsofstories) {
+//       let el = document.createElement("f22-stories");
+//       el.setAttribute("dataUrl", element.url);
+//       mainelementsofstories?.appendChild(el);
+//       document.body.append(mainelementsofstories!);
+//      }
 
-  });
-}
-loaderofdata();
+//   });
+// }
+// loaderofdata();
+
+
+let el = document.createElement('f22-stories')
+el.setAttribute('dataUrl', "https://api.jsonbin.io/v3/b/644b96d98e4aa6225e92895b")
+document.body.append(el)
