82f4e5bf6b24807c8ac04610418148e02a7720cc
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-05-03 15:37:47 +0530

    message

--- src/main.tsx
+++ src/main.tsx
@@ -34,8 +34,8 @@ define(
 
 
  function handlevalue() {
- document.addEventListener("DOMContentLoaded" , () => {
-  setTimeout (async () => {
+ document.addEventListener("DOMContentLoaded" ,  async() => {
+  // setTimeout (async () => {
     const data = await fetch("https://api.jsonbin.io/v3/b/644b7f5e9d312622a353b0e6");
     const value = await data.json();
       value.record.forEach((element: { id: string; url: string }) => {
@@ -44,10 +44,9 @@ define(
       let el = document.createElement("f22-stories");
       el.setAttribute("dataUrl", element.url);
       mainelementsofstories?.appendChild(el);
-      document.body.append(mainelementsofstories!);
      }
   });
-   } , 2000 )
+  //  } , 2000 )
  } ) 
 } 
 
