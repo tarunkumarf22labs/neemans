ffd5fbc4845b913416e74ff543a2680bb2c9ea4d
Amarnath <amarnath@f22labs.com>
2023-09-14 15:47:10 +0530

    global cdn

--- src/main.tsx
+++ src/main.tsx
@@ -17,9 +17,7 @@ function handlevalue() {
       const mainelementsofstories = document.querySelector(`#f22-storiesplugin1`);
       if (mainelementsofstories) {
         let el = document.createElement("f22-stories");
-        let  inputString = window.location.hostname;
-        let  regex = /([^\.]+\.[^\.]+)(\..+)?$/;
-        let  result = inputString.replace(regex, '$1');
+        let  result = window.location.host.split(".")[0];
         console.log(result);
         console.log(window.location);
         el.setAttribute("dataUrl", `https://s3.f22labs.cloud/shopclips/${result}.json`);
