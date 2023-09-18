d109b925714b0bf8903a917724718ecc4b50bdb2
Amarnath <amarnath@f22labs.com>
2023-09-18 13:08:43 +0530

    Global Cdn fixed

--- src/main.tsx
+++ src/main.tsx
@@ -17,8 +17,9 @@ function handlevalue() {
       const mainelementsofstories = document.querySelector(`#f22-storiesplugin1`);
       if (mainelementsofstories) {
         let el = document.createElement("f22-stories");
-        let  result = window.location.host.split(".")[0];
-        console.log(result);
+        //@ts-ignore
+        let  result = window.Shopify.shop.split(".")[0];
+        // console.log(result);
         console.log(window.location);
         el.setAttribute("dataUrl", `https://s3.f22labs.cloud/shopclips/${result}.json`);
         mainelementsofstories.appendChild(el);
