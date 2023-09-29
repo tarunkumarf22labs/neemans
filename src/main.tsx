2e6b6a69bea2e0f653c9c6803d5d6579fcdb1b41
Amarnath <amarnath@f22labs.com>
2023-09-29 19:43:00 +0530

    Ekkatha data added

--- src/main.tsx
+++ src/main.tsx
@@ -18,12 +18,12 @@ function handlevalue() {
       if (mainelementsofstories) {
         let el = document.createElement("f22-stories");
         //@ts-ignore
-        let  result = window.Shopify?.shop?.split(".")[0] || "kikkiza";
+        let  result = window.Shopify?.shop?.split(".")[0] || "ekkatha";
         // let  result = window.Shopify?.shop?.split(".")[0];
         // console.log(result);
         console.log(window.location);
         // el.setAttribute("dataUrl", `https://d2v2xr113se7rz.cloudfront.net/shlipashastra-studio/${result}.json`);
-        el.setAttribute("dataUrl", `https://s3.f22labs.cloud/shopclips/kikkiza.json`);
+        el.setAttribute("dataUrl", `https://s3.f22labs.cloud/shopclips/${result}.json`);
 
         mainelementsofstories.appendChild(el);
       }
